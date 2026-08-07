37bae4b806f5bcb12e0a29a8e494d4c26e33d3fa
Marcos Paulo <marcososx@gmail.com>
2026-08-07 20:26:11 -0300

    Força browser a baixar JS novo (cache-busting ?v=3) + CORS allow-headers no preflight

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ const MAX_ATTEMPTS = 2;
 const CORS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Max-Age': '86400',
 };
 
 const H = { apikey: ANON, Authorization: `Bearer ${ANON}` };
